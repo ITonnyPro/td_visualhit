c579d1da1d46efef016a178e570da9b4c53fba75
ITonnyPro <10687575+ITonnyPro@users.noreply.github.com>
2024-09-16 10:14:05 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,6 @@ lua54 'yes'
 description 'Tonny Visual Hit'
 version '1.0'
 
-shared_script '@es_extended/imports.lua'
-
 client_scripts {
-	'@es_extended/locale.lua',
 	'client.lua'
 }
-
-dependency 'es_extended'
